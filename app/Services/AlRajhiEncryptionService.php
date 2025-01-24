a0e634bf5dbb24c63f1b9671a6d3c4fbefd02721
Hala Ibrahim <halaibrahim867@gmail.com>
2025-01-24 11:31:21 +0200

    add alrajhi payment

---
+++ app/Services/AlRajhiEncryptionService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+class AlRajhiEncryptionService
+{
+    private $key;
+    private $iv;
+
+    public function __construct()
+    {
+        $this->key = env("ALRAJHI_ENCRYPTION_KEY");
+        $this->iv = env("ALRAJHI_IV");
+    }
+
+    public function encrypt($data)
+    {
+        $encrypted = openssl_encrypt(
+            $data,
+            'aes-256-cbc',
+            $this->key,
+            OPENSSL_RAW_DATA,
+            $this->iv
+        );
+
+        return bin2hex($encrypted);
+    }
+
+
+    public function decrypt($data)
+    {
+        $binaryData = hex2bin($data);
+        return openssl_decrypt(
+            $binaryData,
+            'aes-256-cbc',
+            $this->key,
+            OPENSSL_RAW_DATA,
+            $this->iv
+        );
+    }
+}
